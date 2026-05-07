f591e9a6519e18cc1928868bd9863b59efb893dd
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-05-07 22:51:26 +0300

    Remove CryptexFixup.kext enabling from build.py
    Removed the enabling of CryptexFixup.kext from T2-specific bypass kexts.

--- opencore_legacy_patcher/efi_builder/build.py
+++ opencore_legacy_patcher/efi_builder/build.py
@@ -84,15 +84,6 @@ class BuildOpenCore:
             try:
                 logging.info("- Adding T2-specific bypass kexts")
                 # 1. Helper Kexts
-                try:
-                    logging.info("Enabling CryptexFixup.kext")
-                    support.BuildSupport(self.model, self.constants, self.config).enable_kext("CryptexFixup.kext", "1.1.0", self.constants.kext_path)
-                except Exception as e:
-                    logging.error("Injecting CryptexFixup.kext failed because of the following error:")
-                    logging.exception("Stack Trace:") # This prints the full technical error
-                    logging.info("Please try again later.")
-                    sys.exit(3)
-                
                 support.BuildSupport(self.model, self.constants, self.config).enable_kext("WhateverGreen.kext", "1.7.0", self.constants.kext_path)
             
                 # AMFIPass is critical for root patching (GPU drivers) on Tahoe
