c97fee69b42a25870fd8497fa34fa532d178c152
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-06-22 21:19:53 +0300

    Remove corecrypto patch comments and add warnings
    Removed comments about corecrypto patches and added warnings for experimental patches.

--- opencore_legacy_patcher/efi_builder/misc.py
+++ opencore_legacy_patcher/efi_builder/misc.py
@@ -639,6 +639,7 @@ class BuildMiscellaneous:
             sys.exit(3)
         
         if enable_experimental_patches==True: #soll normalerweise dieser Funktion niemals True rückgeben, ohne dass der Benutzer selbst ins Code eingreift
+            # corecrypto-Patches komplett entfernt - diese verstecken das echte Problem und beheben nichts
             # NotebookLLM-generierten Patches, überprüfung und testen erforderlich:
             # bitte beachten Sie, dass dieser Patch noch nicht überprüft ist und kann Kernel Panic oder andere unerwünschte Verhalten verursachen
             # Seien Sie momentan mit diese Patches vorsichtig bevor sie es aktivieren
@@ -686,5 +687,3 @@ class BuildMiscellaneous:
                             "Replace": binascii.unhexlify("C390909090909090909090909090909090")
                         }
                     ])
-                
-                # corecrypto-Patches komplett entfernt - diese verstecken das echte Problem und behebt nichts
